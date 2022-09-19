17b48674ba83dbcf03bfe27be13ebba5a457b43d
matt <wangchixin@mattdeiMac.local>
2022-09-19 21:54:39 +0800

    first commit for ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let sales: number = 123_456_789;
+let course: string = "Typescipt";
+let is_published: boolean = true;
+let level;
+
+level = 1;
+level = "a";
+
+function render(document) {
+  console.log(document);
+}
